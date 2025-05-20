8908cf3c6a12d3b0fdbceffc54fca283dbe8ed23
Roan <Roanorlive659@gmail.com>
2025-05-20 12:36:14 +0200

    Scripts naar de scripts folder gezet

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Wereldkaart met PHP</title>
-  <script type="text/javascript" src="mapdata.js"></script>
-  <script type="text/javascript" src="europemap.js"></script>
+  <script type="text/javascript" src="scripts/mapdata.js"></script>
+  <script type="text/javascript" src="scripts/europemap.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
